eb32aeacbd3c00c231662e8c5a34a54175f59fb9
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-11 21:17:03 +0300

    fetching data from firebase

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -12,13 +12,31 @@
 
 
 <script>
+    import db from './firebaseInit'
+
     export default {
         name: 'dashboard',
 
         data () {
             return {
-
+                employees: []
             }
+        },
+
+        created () {
+            db.collection('employees').get().then(querySnapShot => {
+                querySnapShot.forEach(doc => {
+
+                    const data = {
+                        'id': doc.id,
+                        'employee_id': doc.data().employee_id,
+                        'name': doc.data().name,
+                        'dept': doc.data().dept,
+                        'position': doc.data().position
+                    }
+                    this.employees.push(data)
+                })
+            })
         }
     }
 </script>
