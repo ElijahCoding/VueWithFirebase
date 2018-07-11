043dbc06baacbc83a6a84530c9787d87a82f9c6e
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-11 19:53:39 +0300

    firebase set up

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAegq8omGFF5aZwFGx8VvhnIIOQGsTCG5I",
+    authDomain: "vuefs-prod-36747.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-36747.firebaseio.com",
+    projectId: "vuefs-prod-36747",
+    storageBucket: "vuefs-prod-36747.appspot.com",
+    messagingSenderId: "850166238486"
+}
