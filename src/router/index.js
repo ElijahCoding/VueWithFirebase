ff62b18892e67dd034c5c154d5517b2baa4deb9d
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-11 20:07:55 +0300

    routes and components set up

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Dashboard from '@/components/Dashboard'
+import ViewEmployee from '@/components/ViewEmployee'
+import NewEmployee from '@/components/NewEmployee'
+import EditEmployee from '@/components/EditEmployee'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [
+      {
+        path: '/',
+        name: 'Home',
+        component: Home
+      },
+      {
+        path: '/new',
+        name: 'new-employee',
+        component: NewEmployee
+      },
+      {
+        path: '/:employee_id',
+        name: 'view-employee',
+        component: ViewEmployee
+      },
+      {
+        path: '/edit/:employee_id',
+        name: 'edit-employee',
+        component: EditEmployee
+      }
+    ]
 })
